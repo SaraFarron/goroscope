de23d76de69db642d2e6d1b0a4b158b380458777
NikitaSiulin <n.siulin@rufago.ru>
2023-11-23 19:31:36 +0700

    fixes

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import os
 
 import discord
 import requests
-
-SIGNS = [
-    "Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева", "Весы",
-    "Скорпион", "Стрелец", "Козерог", "Водолей", "Рыбы"
-]
+from const import *
 
 
 def get_goroscope(sign: str):
-    response = requests.get(os.getenv("VK_URL")).json()
+    try:
+        response = requests.get(os.getenv("VK_URL")).json()
+    except requests.RequestException:
+        return VK_ERROR
+
     text = response["response"]["items"][1]["text"]
     texts = text.split("\n \n")
     response = []
 
     for text in texts:
-        if sign.upper() in text.upper():
+        current_sign = text.split(": ")[0]
+        if sign.upper() in current_sign.upper():
             response.append(text)
             break
 
     response = "\n".join(response)
     if not response:
-        return "Что за знаки, что они значат?"
+        return NO_SIGN
     return response
 
 
@@ -37,8 +38,12 @@ class Client(discord.Client):
             return
 
         if client.user.mentioned_in(message):
-            sign = message.content.split(" ")[1]
-            await message.reply(get_goroscope(sign))
+            print(f"User {message.author.id} texted {message.content}")
+            sign = message.content.split(" ")
+            if len(sign) >= 2:
+                await message.reply(get_goroscope(sign[1]))
+            else:
+                await message.reply(WRONG_MESSAGE)
 
 
 if __name__ == "__main__":
