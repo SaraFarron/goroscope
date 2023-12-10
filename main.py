a72fe0e1a101c1288e66af6437d8b96c835f5340
NikitaSiulin <n.siulin@rufago.ru>
2023-12-10 20:28:41 +0700

    fix signs recognition and messages

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ import requests
 from const import *
 
 
+def get_correct_sign(text: str, choice: str):
+    choice = choice.lower()
+    texts = text.split("\n \n")
+    if choice in SIGNS:
+        for text in texts:
+            if text.startswith(SIGNS[choice]):
+                return text
+    return NO_SIGN
+
+
 def get_goroscope(sign: str):
     try:
         response = requests.get(os.getenv("VK_URL")).json()
     except requests.RequestException:
         return VK_ERROR
 
+    # mb add detect is a post is a goroscope?
     text = response["response"]["items"][1]["text"]
-    texts = text.split("\n \n")
-    response = []
+    result = get_correct_sign(text, sign)
 
-    for text in texts:
-        current_sign = text.split(": ")[0]
-        if sign.upper() in current_sign.upper():
-            response.append(text)
-            break
-
-    response = "\n".join(response)
-    if not response:
-        return NO_SIGN
-    return response
+    return result
 
 
 class Client(discord.Client):
@@ -39,7 +40,7 @@ class Client(discord.Client):
             return
 
         if client.user.mentioned_in(message):
-            print(f"User {message.author.id} texted {message.content}")
+            print(f"User {message.author.name} texted '{message.content}'")
             sign = message.content.split(" ")
             if len(sign) >= 2:
                 await message.reply(get_goroscope(sign[1]))
@@ -50,7 +51,6 @@ class Client(discord.Client):
 if __name__ == "__main__":
     intents = discord.Intents.default()
     intents.message_content = True
-
     client = Client(intents=intents)
 
     client.run(os.getenv("DISCORD_TOKEN"))
